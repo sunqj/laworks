c7dad08aee6a644583e8200714f5c4f6ad3345dd
coldbloodx <naughtyboywx@yeah.net>
2012-12-26 19:49:05 +0800

    remove useless web directory

--- web/protected/yiic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../../backup/framework/yii-1.1.12.b600af/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-
-require_once($yiic);
