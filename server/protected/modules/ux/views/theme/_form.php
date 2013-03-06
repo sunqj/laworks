9df9d4033a9729560d7ece704be756f884f1badc
leo@laptop <naughtyboywx@yeah.net>
2013-03-07 01:16:19 +0800

    preview css is ok

--- server/protected/modules/ux/views/theme/_form.php
+++ server/protected/modules/ux/views/theme/_form.php
@@ -26,6 +26,11 @@ $form = $this->beginWidget ( 'CActiveForm', array (
 		<?php echo $form->error($model,'theme_name'); ?>
 	</div>
 	
+	<div class="row">
+		<?php echo $form->labelEx($model,'enterprise_id'); ?>
+		<?php echo $form->dropDownList($model, 'enterprise_id', Enterprise::model()->getEnterpriseList()); ?>
+		<?php echo $form->error($model,'enterprise_id'); ?>
+	</div>
 <?php
 		 //generates following scripts
           /*
@@ -110,28 +115,104 @@ function drawUploadControl($viewObject, $namePrefix)
 	</div>
     <div id="fileDiv" name="fileDiv">
     </div>
-	<div>
-		<label>Theme Preview</label>
-		<?php
-			foreach($columns as $key => $value)
-			{
-				echo "<div id='{$key}Image' name='{$key}Image'></div>";
-			} 
-		?>
-		
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'enterprise_id'); ?>
-		<?php echo $form->dropDownList($model, 'enterprise_id', Enterprise::model()->getEnterpriseList()); ?>
-		<?php echo $form->error($model,'enterprise_id'); ?>
-	</div>
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
-
 <?php $this->endWidget(); ?>
-
+</div>
 </div>
 <!-- form -->
+<style type="text/css">
+.mainFrame {width:480px; margin:0px auto; padding:0px;}
+.banner{float:left;z-index:1;width:480px; margin:0px auto; padding:0px;}
+.logo{margin-left:-480px;}
+.ename{margin-left:-430px;margin-top:5px;}
+.bg{ float:left;z-index:1; display:inline;} 
+.c1{ margin-top: -650px;margin-left: 5px;}
+.c2{ margin-top: -650px;margin-left: 172px; }
+.c3{ margin-top: -650px;margin-left: 340px; margin-right:5px;}
+.c4{ margin-top: -420px;margin-left: 5px;}
+.c5{ margin-top: -420px;margin-left: 172px;}
+.c6{ margin-top: -420px;margin-left: 340px; margin-right:5px;}
+.c7{ margin-top: -190px;margin-left: 5px;}
+.c8{ margin-top: -190px;margin-left: 172px;}
+.c9{ margin-top: -190px;margin-left: 340px; margin-right:5px;}
+
+.c1,.c2,.c3,.c4,.c5,.c6,.c7,.c8,.c9,.logo,.ename
+{
+    text-align:center;float:left;z-index:2;display:inline;*margin-left:0px;
+}
+</style>
+	
+	<div>
+		<label>Theme Preview</label>
+	</div>
+	
+	<div class="mainFrame">
+    <!-- header --!>
+    <div class="banner"><img src="/server/static/theme/banner.png" /></div>
+
+    <!-- logo --!>
+    <div class="logo">
+        <img src="/server/static/theme/lg.png" style="margin-top:5px;max-width:30px"/>
+    </div>
+
+    <!-- enterprise name --!>
+    <div class="ename">
+        企业名称
+    </div>
+
+    <!-- bg --!>
+    <div class="bg"><img src="/server/static/theme/bg.png" /></div>
+
+    <!-- 1st row --!>
+    <div class="c1">
+        <img src="/server/static/theme/c1.png" />
+        </br>
+        工作动态
+    </div>
+    <div class="c2">
+        <img src="/server/static/theme/c2.png" />
+        </br>
+        突发事件
+    </div>
+    <div class="c3">
+        <img src="/server/static/theme/c3.png" />
+        </br>
+        公共信息
+    </div>
+
+    <!-- 2nd row --!>
+    <div class="c4">
+        <img src="/server/static/theme/c4.png" />
+        </br>
+        值班安排
+    </div>
+    <div class="c5">
+        <img src="/server/static/theme/c5.png" />
+        </br>
+        值班要情
+    </div>
+    <div class="c6">
+        <img src="/server/static/theme/c6.png" />
+        </br>
+        视频监控
+    </div>
+
+    <!-- 3rd row --!>
+    <div class="c7" name="c7" id="c7">
+        <img src="/server/static/theme/c7.png" />
+        </br>
+        通讯录
+    </div>
+    <div class="c8" name="o1" id="o1">
+        <img src="/server/static/theme/o1.png" />
+        </br>
+        内部通知
+    </div>
+    <div class="c9" name="o2" id="o2">
+        <img src="/server/static/theme/o2.png" />
+        </br>
+        设置
+    </div>
